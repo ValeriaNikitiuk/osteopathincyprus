0addfccab0105eb97bcf6d69b4498d1e560ff4d1
ValeriaNikitiuk <108237625+ValeriaNikitiuk@users.noreply.github.com>
2023-05-05 17:40:44 +0000

    Deploying to gh-pages from @ ValeriaNikitiuk/osteopathincyprus@d1e3eb6754523d0471398da9dcd78d6b61ecbeac 🚀

--- index.2e06c864.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".gallery-open"),t=document.querySelector("#gallery"),n=document.querySelector(".modal-close");e.addEventListener("click",(function(){t.style.display="block"})),t.addEventListener("click",(function(e){e.target!==t||(t.style.display="none",e.target)})),n.addEventListener("click",(function(){t.style.display="none"}));for(var l=document.querySelectorAll(".item-link"),c=0;c<l.length;c++)l[c].getAttribute("href")===window.location.pathname&&l[c].classList.add("active")}();
-//# sourceMappingURL=index.2e06c864.js.map
