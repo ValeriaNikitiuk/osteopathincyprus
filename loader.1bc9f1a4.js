277961bd1d4801b61717ae200696cb93e38e3cf1
ValeriaNikitiuk <108237625+ValeriaNikitiuk@users.noreply.github.com>
2023-05-06 12:31:15 +0000

    Deploying to gh-pages from @ ValeriaNikitiuk/osteopathincyprus@fd021d599de5451743ba6a733606d21f3414c541 🚀

---
+++ loader.1bc9f1a4.js
@@ -0,0 +1,2 @@
+("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequired7c6.register("kvC6y",(function(e,d){$(window).on("load",(function(){$(".loader_inner").fadeOut(),$(".loader").delay(400).fadeOut("slow")}))}));
+//# sourceMappingURL=loader.1bc9f1a4.js.map
---
+++ loader.4973f2bf.js
@@ -0,0 +1,2 @@
+("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequired7c6.register("gjiCh",(function(e,d){$(window).on("load",(function(){$(".loader_inner").fadeOut(),$(".loader").delay(400).fadeOut("slow")}))}));
+//# sourceMappingURL=loader.4973f2bf.js.map
